df76d011c4e15615e2886f536f18970e730d663b
Alexander <ajroot@stanford.edu>
2025-02-12 17:19:08 -0800

    test DCE on structs + fix IfElse DCE bug

--- src/Opt/DCE.cpp
+++ src/Opt/DCE.cpp
@@ -7,7 +7,6 @@
 #include "IR/WriteLoc.h"
 
 #include "Error.h"
-#include "Utils.h"
 
 #include <map>
 #include <string>
@@ -17,8 +16,8 @@ namespace opt {
 
 namespace {
 
-using UseCountMap = std::map<ir::WriteLoc, uint32_t, ir::WriteLocLessThan>;
-using DepUseCountMap = std::map<ir::WriteLoc, UseCountMap, ir::WriteLocLessThan>;
+using UseCountMap = std::map<std::string, uint32_t>;
+using DepUseCountMap = std::map<std::string, UseCountMap>;
 
 struct ComputeUseCounts : ir::Visitor {
     // How many times is a variable read.
@@ -26,91 +25,87 @@ struct ComputeUseCounts : ir::Visitor {
     // How many times does a variable definition reference another variable.
     DepUseCountMap dependent_use_counts;
     // Name of the current variable whose definition is being traversed.
-    std::optional<ir::WriteLoc> curr_var;
+    std::string curr_var;
 
     void visit(const ir::Var *node) override {
-        ir::WriteLoc loc = read_to_writeloc(node);
-        ++use_counts[loc];
-        if (curr_var.has_value()) {
+        ++use_counts[node->name];
+        if (!curr_var.empty()) {
             // Inside a LetStmt/Assign
-            ++dependent_use_counts[*curr_var][loc];
+            ++dependent_use_counts[curr_var][node->name];
         }
     }
 
     void visit(const ir::Lambda *node) override {
         for (const ir::Lambda::Argument &arg : node->args) {
-            // TODO: std::map::contains ?
-            ir::WriteLoc loc = ir::WriteLoc(arg.name, arg.type);
-            internal_assert(use_counts.find(loc) == use_counts.cend());
-            if (curr_var.has_value()) {
-                const UseCountMap &dep_map = dependent_use_counts[*curr_var];
-                internal_assert(dep_map.find(loc) == dep_map.cend());
+            internal_assert(!use_counts.contains(arg.name));
+            if (!curr_var.empty()) {
+                const UseCountMap &dep_map = dependent_use_counts[curr_var];
+                internal_assert(!dep_map.contains(arg.name));
             }
         }
         // Need to erase use counts of arguments from use count maps.
         ir::Visitor::visit(node);
         for (const ir::Lambda::Argument &arg : node->args) {
-            ir::WriteLoc loc = ir::WriteLoc(arg.name, arg.type);
-            use_counts.erase(loc);
-            if (curr_var.has_value()) {
+            use_counts.erase(arg.name);
+            if (!curr_var.empty()) {
                 // Erase from dependent_use_counts as well.
-                dependent_use_counts[*curr_var].erase(loc);
+                dependent_use_counts[curr_var].erase(arg.name);
             }
         }
     }
 
     void visit(const ir::LetStmt *node) override {
-        internal_assert(!curr_var.has_value())
+        internal_assert(curr_var.empty())
             << "Unexpected nested LetStmt: " << ir::Stmt(node)
-            << " when traversing for: " << *curr_var;
+            << " when traversing for: " << curr_var;
         // TODO(ajr): Should LetStmts just contain a string name for writes? Can
         // never immutably write to an access.
-        internal_assert(!use_counts.contains(node->loc))
+        internal_assert(!use_counts.contains(node->loc.base))
             << "ComputeUseCounts already active for var: " << node->loc;
-        internal_assert(!dependent_use_counts.contains(node->loc))
+        internal_assert(!dependent_use_counts.contains(node->loc.base))
             << "ComputeUseCounts already active for var (dependent): "
             << node->loc;
 
-        use_counts[node->loc] = 0;
-        dependent_use_counts[node->loc] = {};
+        use_counts[node->loc.base] = 0;
+        dependent_use_counts[node->loc.base] = {};
 
-        curr_var = node->loc;
+        curr_var = node->loc.base;
         node->value.accept(this);
-        curr_var.reset();
+        curr_var.clear();
     }
 
     void visit(const ir::Assign *node) override {
-        internal_assert(!curr_var.has_value())
+        internal_assert(curr_var.empty())
             << "Unexpected nested Assign: " << ir::Stmt(node)
-            << " when traversing for: " << *curr_var;
-        internal_assert(!node->mutating || !use_counts.contains(node->loc))
+            << " when traversing for: " << curr_var;
+        internal_assert(!node->mutating || !use_counts.contains(node->loc.base))
             << "ComputeUseCounts already active for var: " << node->loc;
-        internal_assert(!node->mutating || !dependent_use_counts.contains(node->loc))
+        internal_assert(!node->mutating || !dependent_use_counts.contains(node->loc.base))
             << "ComputeUseCounts already active for var (dependent): "
             << node->loc;
 
         if (!node->mutating) {
-            use_counts[node->loc] = 0;
-            dependent_use_counts[node->loc] = {};
+            use_counts[node->loc.base] = 0;
+            dependent_use_counts[node->loc.base] = {};
         }
 
-        curr_var = node->loc;
+        curr_var = node->loc.base;
         node->value.accept(this);
-        curr_var.reset();
+        curr_var.clear();
     }
 
     void visit(const ir::Accumulate *node) override {
-        internal_assert(!curr_var.has_value())
+        internal_assert(curr_var.empty())
             << "Unexpected nested Accumulate: " << ir::Stmt(node)
-            << " when traversing for: " << *curr_var;
-        internal_assert(use_counts.contains(node->loc))
+            << " when traversing for: " << curr_var;
+        internal_assert(use_counts.contains(node->loc.base))
             << "ComputeUseCounts not active for var: " << node->loc;
-        internal_assert(dependent_use_counts.contains(node->loc))
+        internal_assert(dependent_use_counts.contains(node->loc.base))
             << "ComputeUseCounts not active for var (dependent): "
             << node->loc;
-        curr_var = node->loc;
+        curr_var = node->loc.base;
         node->value.accept(this);
-        curr_var.reset();
+        curr_var.clear();
     }
 };
 
@@ -126,7 +121,7 @@ struct DeadCodeElimination : ir::Mutator {
 
     void erase_dependents(const ir::WriteLoc &loc) {
         // Erase it's impact on use_counts.
-        if (const auto cmap = dependent_use_counts.find(loc);
+        if (const auto cmap = dependent_use_counts.find(loc.base);
             cmap != dependent_use_counts.cend()) {
             for (const auto &[var, count] : cmap->second) {
                 internal_assert(use_counts[var] >= count)
@@ -140,7 +135,7 @@ struct DeadCodeElimination : ir::Mutator {
     }
 
     ir::Stmt visit(const ir::LetStmt *node) override {
-        if (use_counts[node->loc] == 0) {
+        if (use_counts[node->loc.base] == 0) {
             erase_dependents(node->loc);
             return ir::Stmt();
         }
@@ -148,7 +143,7 @@ struct DeadCodeElimination : ir::Mutator {
     }
 
     ir::Stmt visit(const ir::Assign *node) override {
-        if (use_counts[node->loc] == 0) {
+        if (use_counts[node->loc.base] == 0) {
             if (!node->mutating) {
                 // Definition of this write loc.
                 erase_dependents(node->loc);
@@ -159,7 +154,7 @@ struct DeadCodeElimination : ir::Mutator {
     }
 
     ir::Stmt visit(const ir::Accumulate *node) override {
-        if (use_counts[node->loc] == 0) {
+        if (use_counts[node->loc.base] == 0) {
             return ir::Stmt();
         }
         return node;
@@ -173,6 +168,8 @@ struct DeadCodeElimination : ir::Mutator {
             return node;
         } else if (!then_body.defined() && !else_body.defined()) {
             return ir::Stmt();
+        } else if (then_body.defined() && else_body.defined()) {
+            return ir::IfElse::make(node->cond, std::move(then_body), std::move(else_body));
         } else if (then_body.defined()) {
             return ir::IfElse::make(node->cond, std::move(then_body));
         } else {
@@ -199,6 +196,7 @@ struct DeadCodeElimination : ir::Mutator {
         }
 
         if (stmts.empty()) {
+            std::cout << "erased: " << ir::Stmt(node) << "\n";
             return ir::Stmt();
         } else if (not_changed) {
             return node;
