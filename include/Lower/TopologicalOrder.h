ddc8925f27e2b9194f9d6f8b1e9b8e0392a01096
Alexander <ajroot@stanford.edu>
2025-02-12 17:41:44 -0800

    mv topo_order to separate file

---
+++ include/Lower/TopologicalOrder.h
@@ -0,0 +1,12 @@
+
+#pragma once
+
+#include "IR/Program.h"
+
+namespace bonsai {
+namespace lower {
+
+std::vector<std::string> func_topological_order(const ir::Program &program, const bool undef_calls);
+
+} // namespace lower
+} // namespace bonsai
