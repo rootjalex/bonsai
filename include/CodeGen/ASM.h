cf22872132b81715152f9de4c51453264b6900b0
Chris Gyurgyik <gyurgyikcp@gmail.com>
2025-02-21 15:14:26 -0800

    initial

--- include/CodeGen/ASM.h
+++ include/CodeGen/ASM.h
@@ -6,7 +6,9 @@
 namespace bonsai {
 namespace codegen {
 
-void to_asm(const std::string &filename, const ir::Program &program,
+// Converts program to ASM via the LLVM pipeline. If `filename` is empty, then
+// prints to standard I/O.
+void to_asm(std::string_view filename, const ir::Program &program,
             CodeGen_LLVM *gen);
 
 } // namespace codegen
