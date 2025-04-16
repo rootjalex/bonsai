d0fd516484686111bc3da077fbb28baf675ade44
Chris Gyurgyik <gyurgyikcp@gmail.com>
2025-04-15 19:16:30 -0700

    remove

--- foo.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <array>
-#include <cstdint>
-
-extern "C" {
-
-int32_t add(int32_t i, int32_t j);
-}
--- main.cpp
+++
@@ -1,5 +0,0 @@
-#include "foo.h"
-
-#include <iostream>
-
-int main() { std::cerr << add(1, 2) << "\n"; }
