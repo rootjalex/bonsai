639d0a55b910d47a3184f36ef4452cbd929aded4
Chris Gyurgyik <gyurgyikcp@gmail.com>
2025-04-17 15:02:31 -0700

    remove rec function

--- include/IR/Analysis.h
+++ include/IR/Analysis.h
@@ -35,8 +35,5 @@ bool contains(const ir::Expr &expr) {
     return checker.found;
 }
 
-// Returns whether this function has a recursive call to itself.
-bool is_recursive(const ir::Function &function);
-
 } // namespace ir
 } // namespace bonsai
