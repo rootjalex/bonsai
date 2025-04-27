087fb293772238097fc9bc660fd96d82e1e2acb0
Alexander <ajroot@stanford.edu>
2025-04-27 00:03:04 -0700

    use Chris's good frame stacks

--- include/IR/Frame.h
+++ include/IR/Frame.h
@@ -9,25 +9,49 @@
 namespace bonsai {
 namespace ir {
 
-template <typename T>
-struct FrameStack {
-    std::list<std::map<std::string, T>> frames;
-
-    T from_frames(const std::string &name) const {
+template <typename K, typename V, typename H = std::less<K>>
+struct MapStack {
+    // Retrieves the variable from this frame stack if it exists, and
+    // {} otherwise.
+    std::optional<V> from_frames(const K &k) const {
         for (auto it = frames.rbegin(); it != frames.rend(); it++) {
             const auto &frame = *it;
-            const auto &found = frame.find(name);
+            const auto &found = frame.find(k);
             if (found != frame.cend()) {
                 return found->second;
             }
         }
-        internal_error << "Cannot get from frame: " << name;
+        return {};
     }
 
-    bool name_in_scope(const std::string &name) const {
+    bool contains(const K &k) const { return from_frames(k).has_value(); }
+
+    void add_to_frame(K k, V v) {
         for (auto it = frames.rbegin(); it != frames.rend(); it++) {
             const auto &frame = *it;
-            const auto &found = frame.find(name);
+            const auto &found = frame.find(k);
+            if (found == frame.end()) {
+                continue;
+            }
+            internal_error << "found duplicate value: " << k;
+        }
+        frames.back()[std::move(k)] = std::move(v);
+    }
+
+    void new_frame() { frames.emplace_back(); }
+
+    void pop_frame() { frames.pop_back(); }
+
+  private:
+    std::vector<std::map<K, V, H>> frames = {{}};
+};
+
+template <typename K, typename H = std::less<K>>
+struct SetStack {
+    bool contains(const K &k) const {
+        for (auto it = frames.rbegin(); it != frames.rend(); it++) {
+            const auto &frame = *it;
+            const auto &found = frame.find(k);
             if (found != frame.cend()) {
                 return true;
             }
@@ -35,20 +59,29 @@ struct FrameStack {
         return false;
     }
 
-    void add_to_frame(const std::string &name, T value) {
+    void add_to_frame(K k) {
         for (auto it = frames.rbegin(); it != frames.rend(); it++) {
             const auto &frame = *it;
-            const auto &found = frame.find(name);
-            internal_assert(found == frame.cend())
-                << name << " shadows another variable (of the same name)";
+            const auto &found = frame.find(k);
+            if (found == frame.end()) {
+                continue;
+            }
+            internal_error << "found duplicate value: " << k;
         }
-        frames.back()[name] = value;
+        frames.back().insert(k);
     }
 
     void new_frame() { frames.emplace_back(); }
 
     void pop_frame() { frames.pop_back(); }
+
+  private:
+    std::vector<std::set<K, H, std::allocator<K>>> frames = {{}};
 };
+
+// Mapping from string identifiers, e.g., variables names, to some type T.
+template <typename T>
+using FrameStack = MapStack<std::string, T>;
 
 } // namespace ir
 } // namespace bonsai
