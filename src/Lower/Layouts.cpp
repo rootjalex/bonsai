25d81818c839e1a680a4a7dc245e760b7ea557c2
Chris Gyurgyik <gyurgyikcp@gmail.com>
2025-05-01 10:25:09 -0700

    x

--- src/Lower/Layouts.cpp
+++ src/Lower/Layouts.cpp
@@ -114,12 +114,10 @@ ir::Expr fill(const ir::FrameStack<ir::Expr> &frames, const ir::Expr &expr) {
                     ir::Function_t::make(std::move(ret_type), func->arg_types),
                     var->name);
             }
-            internal_assert(frames.name_in_scope(var->name))
+            std::optional<ir::Expr> expr = frames.from_frames(var->name);
+            internal_assert(expr.has_value())
                 << "Materialization fill cannot find: " << var->name;
-            if (frames.name_in_scope(var->name)) {
-                return frames.from_frames(var->name);
-            }
-            return var;
+            return *expr;
         }
     };
     return Rewrite(frames).mutate(expr);
