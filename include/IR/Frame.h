ebe103a02d77af429d29d1dcf7555ec24d5c3cc7
Chris Gyurgyik <gyurgyikcp@gmail.com>
2025-05-03 13:07:36 -0700

    x

--- include/IR/Frame.h
+++ include/IR/Frame.h
@@ -1,13 +1,21 @@
 #pragma once
 
-#include <list>
 #include <map>
+#include <set>
 #include <string>
+#include <type_traits>
+#include <vector>
 
 #include "Error.h"
 
 namespace bonsai {
 namespace ir {
+namespace {
+// Whether T is an ir::Expr or ir::Stmt.
+template <typename T>
+static constexpr bool IsBonsaiIR =
+    std::is_same_v<T, ir::Expr> || std::is_same_v<T, ir::Stmt>;
+} // namespace
 
 // Maintains a stack of scopes, where each frame is a map from some key type K
 // to some value type V. This is useful for doing analysis within scopes.
@@ -37,16 +45,13 @@ struct MapStack {
         return {};
     }
 
-    // Whether T is an ir::Expr or ir::Stmt.
-    template <typename T>
-    static constexpr bool I =
-        std::is_same_v<T, ir::Expr> || std::is_same_v<T, ir::Stmt>;
-
     // Returns the value at key `k` if found, and nullptr otherwise. This is a
     // safe way to update the value at key `k`. (We opt against using iterators
     // for simplicity.)
     template <typename T = V>
-    typename std::enable_if<!I<T>, V>::type *find(const K &k) {
+    V *find(const K &k)
+        requires(!IsBonsaiIR<V>)
+    {
         for (auto it = frames.rbegin(); it != frames.rend(); it++) {
             auto &frame = *it;
             auto found = frame.find(k);
@@ -86,20 +91,35 @@ struct MapStack {
 // Similar to MapStack, but only inserts keys.
 template <typename K, typename H = std::less<K>>
 struct SetStack {
-    bool contains(const K &k) const { return from_frames(k).has_value(); }
+    bool contains(const K &k) const {
+        for (auto it = frames.rbegin(); it != frames.rend(); it++) {
+            const auto &frame = *it;
+            const auto &found = frame.find(k);
+            if (found != frame.cend()) {
+                return true;
+            }
+        }
+        return false;
+    }
 
-    bool empty() const { return frames.empty(); }
+    void add_to_frame(K k) {
+        for (auto it = frames.rbegin(); it != frames.rend(); it++) {
+            const auto &frame = *it;
+            const auto &found = frame.find(k);
+            if (found == frame.end()) {
+                continue;
+            }
+            internal_error << "found duplicate value: " << k;
+        }
+        frames.back().insert(k);
+    }
 
-    void add_to_frame(K k) { frames.add_to_frame(std::move(k), false); }
-
-    void push_frame() { frames.push_frame(); }
+    void push_frame() { frames.emplace_back(); }
 
     void pop_frame() { frames.pop_back(); }
 
   private:
-    // TODO(cgyurgyik): The "value" here is dead. Eventually this should just be
-    // a set, but I'm keeping it this way as we iterate on the design of this.
-    ir::MapStack<K, bool, H> frames;
+    std::vector<std::set<K, H, std::allocator<K>>> frames = {{}};
 };
 
 } // namespace ir
