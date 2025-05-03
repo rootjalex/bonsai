be862f3bbcf41b1cf5589347590047c65f1066a5
Alexander <ajroot@stanford.edu>
2025-05-02 18:59:25 -0700

    pretty ball

--- apps/rtiow/main.h
+++ apps/rtiow/main.h
@@ -5,5 +5,4 @@
 extern "C" {
 
 int32_t*** image(const int32_t width);
-int32_t*** image__(const int32_t width);
 }
--- apps/rtiow/test.sh
+++ apps/rtiow/test.sh
@@ -6,6 +6,6 @@ cd apps/rtiow
 ../../build/compiler -i main.bonsai -b cpp -o main
 ../../build/compiler -i main.bonsai -b llvm -o main.ll
 clang++ -std=c++20 -O3 main_hook.cpp main.o -o bonsai.out
-time ./bonsai.out 400 > bimage.ppm
+time ./bonsai.out 800 > bimage.ppm
 clang++ -std=c++20 -O3 compare.cpp -o compare.out
-time ./compare.out 400 > image.ppm
+time ./compare.out 800 > image.ppm
