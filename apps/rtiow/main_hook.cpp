e722e58697f335c5691b9cddf5dda04326ff752c
Alexander <ajroot@stanford.edu>
2025-05-04 22:38:26 -0700

    depth of field

--- apps/rtiow/main_hook.cpp
+++ apps/rtiow/main_hook.cpp
@@ -133,6 +133,8 @@ int main(int argc, char *argv[]) {
     cam.lookfrom = {-2, 2, 1};
     cam.lookat = {0, 0, -1};
     cam.vup = {0, 1, 0};
+    cam.defocus_angle = 10.0;
+    cam.focus_dist = 3.4;
 
     // Render
     int *im = (int *)image(cam, tree);
