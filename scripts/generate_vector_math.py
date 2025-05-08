8cc2c3be90d7fb85fe927118d2c5047064488a3f
Chris Gyurgyik <gyurgyikcp@gmail.com>
2025-05-07 19:43:31 -0700

    update script

--- scripts/generate_vector_math.py
+++ scripts/generate_vector_math.py
@@ -2,7 +2,7 @@ PROLOGUE = "__forceinline__ __host__ __device__"
 TYPE_TO_RETURN_TYPE = {"int": "int32_t", "uint": "uint32_t"}
 TYPES = ["int", "uint", "float"]
 LANES = [2, 3, 4]
-FIELDS = ["x", "y", "z", "w"]
+F = ["x", "y", "z", "w"]
 
 INDEX_TYPE = "uint32_t"
 
@@ -16,14 +16,14 @@ def generate_vector_reduce():
     print("////////////////////////////////////////////////////////////////////////////////")
     print()
     op_to_symbol = {"sum": "+", "mul": "*"}
-    input = "v"
+    I = "v"
     for op, symbol in op_to_symbol.items():
         for type in TYPES:
             for n in LANES:
                 return_type = TYPE_TO_RETURN_TYPE[type] if type in TYPE_TO_RETURN_TYPE else type
-                function_name = f"{op}({type}{n} {input})"
+                function_name = f"{op}({type}{n} {I})"
                 expression = symbol.join(
-                    [f"{input}.{FIELDS[i]}" for i in range(n)]
+                    [f"{I}.{F[i]}" for i in range(n)]
                 )
                 body = f"return {expression};"
                 function_definition = f"{PROLOGUE} {return_type} {function_name} {{ {body} }}"
@@ -41,19 +41,19 @@ def generate_vector_idxmax():
     print()
     op_to_symbol = {"idxmax": ">", "idxmin": "<"}
     return_type = INDEX_TYPE
-    input = "v"
+    I = "v"
     for op, symbol in op_to_symbol.items():
         for type in TYPES:
             for n in LANES:
-                function_name = f"{op}({type}{n} {input})"
+                function_name = f"{op}({type}{n} {I})"
                 body = f"""{return_type} idx = 0u;"""
                 match n:
                     case 2:
-                        body += f"""idx = ({input}.{FIELDS[1]} {symbol} {input}.{FIELDS[0]}) ? 1u : 0u;"""
+                        body += f"""idx = ({I}.{F[1]} {symbol} {I}.{F[0]}) ? 1u : 0u;"""
                     case 3:
-                        body += f"""if ({input}.{FIELDS[1]} {symbol} {input}.{FIELDS[0]}) idx = 1u; if ({input}.{FIELDS[2]} {symbol} (idx == 0u ? {input}.{FIELDS[0]} : (idx == 1u ? {input}.{FIELDS[1]} : {input}.{FIELDS[0]}))) idx = 2u;"""
+                        body += f"""if ({I}.{F[1]} {symbol} {I}.{F[0]}) idx = 1u; if ({I}.{F[2]} {symbol} (idx == 0u ? {I}.{F[0]} : (idx == 1u ? {I}.{F[1]} : {I}.{F[0]}))) idx = 2u;"""
                     case 4:
-                        body += f"""if ({input}.{FIELDS[1]} {symbol} {input}.{FIELDS[0]}) idx = 1u; if ({input}.{FIELDS[2]} {symbol} (idx == 0u ? {input}.{FIELDS[0]} : {input}.{FIELDS[1]})) idx = 2u; if ({input}.{FIELDS[3]} {symbol} (idx == 0u ? {input}.{FIELDS[0]} : (idx == 1u ? {input}.{FIELDS[1]} : {input}.{FIELDS[2]}))) idx = 3u;"""
+                        body += f"""if ({I}.{F[1]} {symbol} {I}.{F[0]}) idx = 1u; if ({I}.{F[2]} {symbol} (idx == 0u ? {I}.{F[0]} : {I}.{F[1]})) idx = 2u; if ({I}.{F[3]} {symbol} (idx == 0u ? {I}.{F[0]} : (idx == 1u ? {I}.{F[1]} : {I}.{F[2]}))) idx = 3u;"""
                 body += "\n"
                 body += "return idx;"
                 body += "\n"
@@ -76,12 +76,12 @@ def generate_vector_shuffle():
             func_name = f" shuffle({vector_type} v, std::initializer_list<{INDEX_TYPE}> indices)"
             body = f"""{{ {vector_type} r; auto it = indices.begin();"""
             for i in range(n):
-                body += f"""switch (*it++) {{ case 0: r.{FIELDS[i]} = v.x; break; case 1: r.{FIELDS[i]} = v.y; break;"""
+                body += f"""switch (*it++) {{ case 0: r.{F[i]} = v.x; break; case 1: r.{F[i]} = v.y; break;"""
                 if n > 2:
-                    body += f""" case 2: r.{FIELDS[i]} = v.z; break;"""
+                    body += f""" case 2: r.{F[i]} = v.z; break;"""
                 if n > 3:
-                    body += f""" case 3: r.{FIELDS[i]} = v.w; break;"""
-                body += f""" default: abort(); }} """
+                    body += f""" case 3: r.{F[i]} = v.w; break;"""
+                body += f""" default: __builtin_unreachable(); }} """
             body += f"""return r; }}"""
             print(f"{PROLOGUE} {vector_type} {func_name} {body}")
         print()
