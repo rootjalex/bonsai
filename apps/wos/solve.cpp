8fe0187722776965971711ac6bfacd813aa6e9a6
Alexander <ajroot@stanford.edu>
2025-05-21 13:49:35 -0700

    add no GUI testing to solve.cpp

--- apps/wos/solve.cpp
+++ apps/wos/solve.cpp
@@ -1318,6 +1318,252 @@ void guiCallback(const std::vector<Vector<DIM>> &meshPositions,
     ImGui::PopItemWidth();
 }
 
+#define NO_GUI 1
+
+#ifdef NO_GUI
+template <size_t DIM>
+void no_GUI(const std::vector<Vector<DIM>> &meshPositions,
+            const std::vector<bool> &isBoundaryVertex,
+            const zombie::GeometricQueries<DIM> &geometricQueries,
+            const zombie::WalkOnSpheres<float, DIM> &walkOnSpheres,
+            zombie::PDE<float, DIM> &pde, std::vector<int> &nWalks,
+            std::vector<zombie::SamplePoint<float, DIM>> &samplePoints,
+            std::vector<float> &walkOnSpheresSolution) {
+    // make ui elements 150 pixels wide, instead of full width
+    if (ignoreSourceContribution) {
+        clearResults<DIM>(samplePoints);
+    }
+
+    if (ignoreDirichletContribution) {
+        clearResults<DIM>(samplePoints);
+    }
+
+    if (!domainIsOpen) {
+        if (estimateOnSlicePlane) {
+            if (estimateOnSlicePlane) {
+                // initialize sample points as slice plane positions
+                if (polyscope::state::slicePlanes.size() == 0) {
+                    polyscope::SlicePlane *psPlane =
+                        polyscope::addSceneSlicePlane();
+                    psPlane->setDrawPlane(false);
+                    psPlane->setDrawWidget(false);
+                    psPlane->setActive(false);
+                    psPlane->setPose(glm::vec3{0., 0., 0.},
+                                     glm::vec3{0., 0, 1.});
+                }
+
+                std::vector<Vector<DIM>> slicePlanePositions;
+                std::vector<std::vector<size_t>> slicePlaneIndices;
+                glm::mat4 currentTransform =
+                    polyscope::state::slicePlanes[0]->getTransform();
+                addSlicePlane<DIM>(geometricQueries.domainMin,
+                                   geometricQueries.domainMax, currentTransform,
+                                   slicePlaneResolution, slicePlanePositions,
+                                   slicePlaneIndices);
+                addSlicePlaneSampleAndEvaluationPoints<DIM>(
+                    slicePlanePositions, slicePlaneIndices, geometricQueries,
+                    nWalksForSamplePts, estimateGradients, nWalks,
+                    samplePoints);
+
+                if (DIM == 2) {
+                    polyscope::SurfaceMesh *mesh =
+                        polyscope::getSurfaceMesh("Mesh");
+                    mesh->removeQuantity("Solution Estimate");
+                    mesh->removeQuantity("Solution Variance");
+                    mesh->removeQuantity("Gradient Estimate");
+                    mesh->removeQuantity("Mean Walk Length");
+                } else if (DIM == 3) {
+                    polyscope::VolumeMesh *mesh =
+                        polyscope::getVolumeMesh("Mesh");
+                    mesh->removeQuantity("Solution Estimate");
+                    mesh->removeQuantity("Solution Variance");
+                    mesh->removeQuantity("Gradient Estimate");
+                    mesh->removeQuantity("Mean Walk Length");
+                }
+            } else {
+                // initialize sample points as mesh positions
+                addMeshSampleAndEvaluationPoints<DIM>(
+                    meshPositions, isBoundaryVertex, geometricQueries,
+                    nWalksForSamplePts, estimateGradients, nWalks,
+                    samplePoints);
+
+                polyscope::removeStructure("Slice Plane", false);
+                polyscope::removeLastSceneSlicePlane();
+            }
+
+            plotSamplePoints<DIM>(samplePoints);
+
+            nWalksTakenForSamplePts = 0;
+        }
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////
+    if (!estimateOnSlicePlane || (estimateOnSlicePlane && solveDoubleSided)) {
+        std::fill(nWalks.begin(), nWalks.end(), nWalksForSamplePts);
+        if (estimateGradients) {
+            for (int i = 0; i < (int)samplePoints.size(); i++) {
+                samplePoints[i].estimationQuantity = getEstimationQuantity(
+                    samplePoints[i].type, estimateGradients);
+            }
+
+            clearResults<DIM>(samplePoints);
+        }
+
+        // solve with walk on spheres
+        if (true) {
+            // estimate pointwise
+            // ProgressBar pb(samplePoints.size());
+            std::function<void(int, int)> reportProgress =
+                [](int i, int tid) -> void { return; };
+            // getReportProgressCallback(pb);
+
+            SamplePoint *bonsai_pts = (SamplePoint *)malloc(
+                sizeof(SamplePoint) * samplePoints.size());
+            if constexpr (DIM == 3) {
+                for (uint64_t i = 0; i < samplePoints.size(); i++) {
+                    bonsai_pts[i].pt = {samplePoints[i].pt(0),
+                                        samplePoints[i].pt(1),
+                                        samplePoints[i].pt(2)};
+                    bonsai_pts[i].normal = {samplePoints[i].normal(0),
+                                            samplePoints[i].normal(1),
+                                            samplePoints[i].normal(2)};
+                    bonsai_pts[i].pdf = samplePoints[i].pdf;
+                    bonsai_pts[i].distToAbs =
+                        samplePoints[i].distToAbsorbingBoundary;
+                    bonsai_pts[i].distToRefl =
+                        samplePoints[i].distToReflectingBoundary;
+                    bonsai_pts[i].type_and_quantity = 0;
+                    if (samplePoints[i].type == zombie::SampleType::InDomain) {
+                        bonsai_pts[i].type_and_quantity |= 0;
+                    } else if (samplePoints[i].type ==
+                               zombie::SampleType::OnAbsorbingBoundary) {
+                        bonsai_pts[i].type_and_quantity |= 1;
+                    } else if (samplePoints[i].type ==
+                               zombie::SampleType::OnReflectingBoundary) {
+                        bonsai_pts[i].type_and_quantity |= 2;
+                    }
+
+                    if (samplePoints[i].estimationQuantity ==
+                        zombie::EstimationQuantity::Solution) {
+                        bonsai_pts[i].type_and_quantity |= 0;
+                    } else if (samplePoints[i].estimationQuantity ==
+                               zombie::EstimationQuantity::
+                                   SolutionAndGradient) {
+                        bonsai_pts[i].type_and_quantity |= 4;
+                    } else if (samplePoints[i].estimationQuantity ==
+                               zombie::EstimationQuantity::None) {
+                        bonsai_pts[i].type_and_quantity |= 8;
+                    }
+
+                    if (samplePoints[i].estimateBoundaryNormalAligned) {
+                        bonsai_pts[i].type_and_quantity |= 16;
+                    }
+                }
+            }
+
+            // TODO(ajr): THIS IS WHAT WE NEED.
+            zombie::WalkSettings walkSettings(
+                epsilonShell, 0.0f, 0.0f, russianRouletteThreshold,
+                splittingThreshold, maxWalkLength, stepsBeforeApplyingTikhonov,
+                maxWalkLength, solveDoubleSided,
+                !disableGradientControlVariates,
+                !disableGradientAntitheticVariates,
+                useCosineSamplingForDirectionalDerivatives,
+                ignoreDirichletContribution, false, ignoreSourceContribution,
+                printLogs);
+            auto start = std::chrono::high_resolution_clock::now();
+            walkOnSpheres.solve(pde, walkSettings, nWalks, samplePoints,
+                                runSingleThreaded, reportProgress);
+            auto end = std::chrono::high_resolution_clock::now();
+            std::chrono::duration<double> elapsed = end - start;
+            std::cout << "walkOnSpheres.solve took " << elapsed.count()
+                      << " seconds.\n";
+            std::cout << "TBB max concurrency: "
+                      << tbb::this_task_arena::max_concurrency() << std::endl;
+            // pb.finish();
+
+            Statistics *bonsai_solution = nullptr;
+            // constexpr uint64_t bonsai_fixed_count = 200;
+
+            if constexpr (DIM == 3) {
+                WalkSettings bonsai_ws;
+                bonsai_ws.box = box;
+                bonsai_ws.epsShellAbs = epsilonShell;
+                bonsai_ws.epsShellRefl = 0.0; // TODO: not used
+                bonsai_ws.silPrecision = 0.0; // TODO: not used
+                bonsai_ws.russianRouletteThreshold = russianRouletteThreshold;
+                bonsai_ws.maxWalkLength = maxWalkLength;
+                bonsai_ws.stepsBeforeApplyingTikhonov =
+                    stepsBeforeApplyingTikhonov; // TODO: not used
+                bonsai_ws.flags =
+                    (int)solveDoubleSided |
+                    (((int)!disableGradientControlVariates) << 1) |
+                    (((int)!disableGradientAntitheticVariates) << 2) |
+                    (((int)useCosineSamplingForDirectionalDerivatives) << 3) |
+                    (((int)ignoreDirichletContribution) << 4) |
+                    // ignoreReflectingBoundaryCondition is always false?
+                    (((int)ignoreSourceContribution) << 6) |
+                    (((int)printLogs) << 7);
+                PDE bonsai_pde;
+                bonsai_pde.absCoeff = pde.absorptionCoeff;
+                bonsai_pde.freq = pde.freq;
+
+                const uint32_t nSamplePts = samplePoints.size();
+                // const uint64_t nSamplePts = bonsai_fixed_count;
+                auto start = std::chrono::high_resolution_clock::now();
+                std::cout << nSamplePts << "pts" << std::endl;
+
+                std::cout << nWalksForSamplePts << " walks per pt" << std::endl;
+
+                bonsai_solution = solve(bonsai_pde, bonsai_ws, nSamplePts,
+                                        bonsai_pts, nWalksForSamplePts, tree);
+
+                auto end = std::chrono::high_resolution_clock::now();
+                std::chrono::duration<double> elapsed = end - start;
+                std::cout << "bonsai.solve took " << elapsed.count()
+                          << " seconds.\n";
+            }
+
+            // plot results
+            std::vector<float> firstSphereRadii;
+            for (int i = 0; i < (int)samplePoints.size(); i++) {
+                if (bonsai_solution) {
+                    firstSphereRadii.push_back(
+                        bonsai_solution[i].firstSphereRadius);
+                } else {
+                    firstSphereRadii.push_back(
+                        samplePoints[i].firstSphereRadius);
+                }
+            }
+
+            auto pointCloud = polyscope::getPointCloud("Sample Points");
+            pointCloud->addScalarQuantity("First Sphere Radii",
+                                          firstSphereRadii);
+            pointCloud->setPointRadiusQuantity("First Sphere Radii");
+
+            // mask out sample values close to the boundary
+            maskWalkOnSpheresEstimates(boundaryDistanceMask, samplePoints);
+
+            // plot sample values
+            plotWalkOnSpheresEstimates<DIM>(samplePoints, estimateOnSlicePlane,
+                                            walkOnSpheresSolution);
+
+            if (bonsai_solution) {
+                // mask out sample values close to the boundary
+                maskWalkOnSpheresEstimatesBonsai(boundaryDistanceMask,
+                                                 samplePoints, bonsai_solution);
+
+                // plot sample values
+                plotWalkOnSpheresEstimatesBonsai(
+                    bonsai_solution, samplePoints.size(), estimateOnSlicePlane,
+                    walkOnSpheresSolution);
+            }
+            nWalksTakenForSamplePts += nWalksForSamplePts;
+        }
+    }
+}
+#endif
+
 template <size_t DIM>
 void visualizeScene(const std::vector<Vector<DIM>> &meshPositions,
                     const std::vector<Vector<DIM>> &boundaryPositions,
@@ -1390,6 +1636,12 @@ void visualizeScene(const std::vector<Vector<DIM>> &meshPositions,
 
     // bind gui callback
     std::vector<float> walkOnSpheresSolution;
+#ifdef NO_GUI
+    no_GUI<DIM>(std::cref(meshPositions), std::cref(isBoundaryVertex),
+                std::cref(geometricQueries), std::cref(walkOnSpheres),
+                std::ref(pde), std::ref(nWalks), std::ref(samplePoints),
+                std::ref(walkOnSpheresSolution));
+#else
 #ifdef USE_FEM
     std::vector<float> femSolution;
     polyscope::state::userCallback =
@@ -1405,9 +1657,9 @@ void visualizeScene(const std::vector<Vector<DIM>> &meshPositions,
                   std::cref(walkOnSpheres), std::ref(pde), std::ref(nWalks),
                   std::ref(samplePoints), std::ref(walkOnSpheresSolution));
 #endif
-
     // give control to polyscope gui
     polyscope::show();
+#endif
 }
 
 template <size_t DIM>
