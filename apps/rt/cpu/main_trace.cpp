60e7387d15cdc4c699d1a425e455fdd483964882
Chris Gyurgyik <gyurgyikcp@gmail.com>
2025-09-26 09:26:14 -0700

    add more rays

--- apps/rt/cpu/main_trace.cpp
+++ apps/rt/cpu/main_trace.cpp
@@ -90,8 +90,8 @@ void run_test(const std::string &object) {
     free_canonical_tree(canonical_tree);
 
     std::vector<int64_t> ray_counts = {
-        1 << 8,  1 << 9,  1 << 10, 1 << 11, 1 << 12,
-        1 << 13, 1 << 14, 1 << 15, 1 << 16,
+        1 << 8,  1 << 9,  1 << 10, 1 << 11, 1 << 12, 1 << 13,
+        1 << 14, 1 << 15, 1 << 16, 1 << 17, 1 << 18,
     };
     for (const int64_t ray_count : ray_counts) {
         std::cout << ray_count << std::endl;
