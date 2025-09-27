9695d140706ecfb4873d7756404465eb751307a1
Chris Gyurgyik <gyurgyikcp@gmail.com>
2025-09-26 18:15:14 -0700

    all in 1

--- apps/rtiow/cuda/main.cu
+++ apps/rtiow/cuda/main.cu
@@ -1,6 +1,3 @@
-#include "canonical_tree.h"
-#include "util.h"
-
 #include "helpers.h"
 #include "rtiow.h"
 
@@ -11,6 +8,227 @@
 #include <iostream>
 #include <random>
 #include <vector>
+
+namespace {
+
+__host__ inline float random_float() {
+    static std::uniform_real_distribution<float> distribution(0.0, 1.0);
+    static std::mt19937 generator;
+    return distribution(generator);
+}
+
+__host__ inline float random_float(float min, float max) {
+    // Returns a random real in [min,max).
+    return min + (max - min) * random_float();
+}
+
+__host__ Camera setup_camera() {
+    Camera camera;
+    camera.aspect_ratio = 16.0 / 9.0;
+    camera.width = 1200; // makes height = 675
+    camera.samples_per_pixel = 50;
+    camera.max_depth = 20;
+
+    camera.vfov = 20;
+    camera.lookfrom = {13, 2, 3};
+    camera.lookat = {0, 0, 0};
+    camera.vup = {0, 1, 0};
+
+    camera.defocus_angle = 0.6;
+    camera.focus_dist = 10.0;
+    return camera;
+}
+
+__host__ std::vector<MaterialSphere> setup_spheres() {
+    constexpr uint32_t LAMBERTIAN = 0;
+    constexpr uint32_t METAL = 1;
+    constexpr uint32_t DIALECTRIC = 2;
+
+    std::vector<MaterialSphere> spheres{
+        // Ground
+        {Sphere{{0, -1000, 0}, 1000}, LAMBERTIAN, {0.5, 0.5, 0.5}, 0.0},
+        {Sphere{{0, 1, 0}, 1}, DIALECTRIC, {0, 0, 0}, 1.5},
+        {Sphere{{-4, 1, 0}, 1}, LAMBERTIAN, {0.4, 0.2, 0.1}, 0.0},
+        {Sphere{{4, 1, 0}, 1}, METAL, {0.7, 0.6, 0.5}, 0.0},
+    };
+
+    for (int a = -11; a < 11; a++) {
+        for (int b = -11; b < 11; b++) {
+            auto choose_mat = random_float();
+            vec3_float center = {static_cast<float>(a + 0.9 * random_float()),
+                                 0.2,
+                                 static_cast<float>(b + 0.9 * random_float())};
+
+            vec3_float a = {4, 0.2, 0};
+
+            vec3_float diff = (center - a);
+            float len =
+                sqrt(diff.x * diff.x + diff.y * diff.y + diff.z * diff.z);
+
+            if (len > 0.9) {
+                if (choose_mat < 0.8) {
+                    // diffuse
+                    vec3_float r0 = {random_float(), random_float(),
+                                     random_float()};
+                    vec3_float r1 = {random_float(), random_float(),
+                                     random_float()};
+                    auto albedo = r0 * r1;
+                    spheres.push_back(
+                        {Sphere{center, 0.2}, LAMBERTIAN, albedo, 0.0});
+                } else if (choose_mat < 0.95) {
+                    // metal
+                    vec3_float albedo = {random_float(0.5, 1),
+                                         random_float(0.5, 1),
+                                         random_float(0.5, 1)};
+                    float fuzz = random_float(0, 0.5);
+                    spheres.push_back(
+                        {Sphere{center, 0.2}, METAL, albedo, fuzz});
+                } else {
+                    // glass
+                    spheres.push_back(
+                        {Sphere{center, 0.2}, DIALECTRIC, {0, 0, 0}, 1.5});
+                }
+            }
+        }
+    }
+    return spheres;
+}
+
+__host__ int save_image(int64_t image_height, int64_t image_width,
+                        const int *image, const std::string &output_filename) {
+    std::ofstream out(output_filename);
+
+    if (!out) {
+        std::cerr << "Error: Cannot open output file: `" << output_filename
+                  << "` for writing\n";
+        return 1;
+    }
+
+    out << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+    for (int j = 0; j < image_height; j++) {
+        for (int i = 0; i < image_width; i++) {
+            int ir = image[(j * image_width + i) * 3 + 0];
+            int ig = image[(j * image_width + i) * 3 + 1];
+            int ib = image[(j * image_width + i) * 3 + 2];
+            out << ir << ' ' << ig << ' ' << ib << '\n';
+        }
+    }
+    return 0;
+}
+
+__host__ Sphere get_bounding_sphere(const BVH *node) {
+    if (std::holds_alternative<Interior>(*node)) {
+        const Interior &interior = std::get<Interior>(*node);
+        return {interior.center, interior.radius};
+    }
+    if (std::holds_alternative<Leaf>(*node)) {
+        const Leaf &leaf = std::get<Leaf>(*node);
+        return {leaf.center, leaf.radius};
+    }
+    assert(false && "unexpected");
+}
+
+__host__ void free_canonical_tree(BVH *node) {
+    if (std::holds_alternative<Interior>(*node)) {
+        Interior &interior = std::get<Interior>(*node);
+        free_canonical_tree(interior.left);
+        free_canonical_tree(interior.right);
+        free(&interior);
+        return;
+    }
+
+    if (std::holds_alternative<Leaf>(*node)) {
+        Leaf &leaf = std::get<Leaf>(*node);
+        free(leaf.data);
+        free(&leaf);
+        return;
+    }
+
+    assert(false && "unexpected");
+}
+
+// Builds the canonical tree using a median split.
+__host__ BVH *build_canonical_tree(std::vector<MaterialSphere> &spheres) {
+    constexpr uint32_t MAX_TREE_DEPTH = 64;
+    std::function<BVH *(uint32_t, uint32_t, uint32_t)> partition =
+        [&](uint32_t low, uint32_t high, uint32_t depth = 0) -> BVH * {
+        assert(depth < MAX_TREE_DEPTH);
+        uint32_t count = high - low;
+        if (count <= 2) {
+            vec3_float center = spheres[low].s.center;
+            f32 radius = spheres[low].s.radius;
+            if (count == 2) {
+                Sphere merged =
+                    bounding_sphere(&spheres[low].s, &spheres[low + 1].s);
+                center = merged.center;
+                radius = merged.radius;
+            }
+            auto *data =
+                (MaterialSphere *)(malloc(sizeof(MaterialSphere) * count));
+            for (int i = 0; i < count; ++i) {
+                data[i] = spheres[low + i];
+            }
+            return new BVH(Leaf{
+                .center = center,
+                .radius = radius,
+                .nprims = static_cast<uint8_t>(count),
+                .data = data,
+            });
+        }
+
+        // Internal node
+        vec3_float min_bound = spheres[low].s.center;
+        vec3_float max_bound = spheres[low].s.center;
+
+        for (uint32_t i = low + 1; i < high; ++i) {
+            min_bound = min(min_bound, spheres[i].s.center);
+            max_bound = max(max_bound, spheres[i].s.center);
+        }
+
+        // Choose axis with greatest extent
+        vec3_float extent = max_bound - min_bound;
+        int axis = 0;
+        float ex = extent.x;
+        float ey = extent.y;
+        float ez = extent.z;
+
+        if (ey > ex)
+            axis = 1;
+        if (ez > ((axis == 0) ? ex : ey))
+            axis = 2;
+
+        // Partition at midpoint along chosen axis
+        auto mid_it = spheres.begin() + low + count / 2;
+        std::nth_element(spheres.begin() + low, mid_it, spheres.begin() + high,
+                         [&](const MaterialSphere &a, const MaterialSphere &b) {
+                             if (axis == 0)
+                                 return a.s.center.x < b.s.center.x;
+                             if (axis == 1)
+                                 return a.s.center.y < b.s.center.y;
+                             return a.s.center.z < b.s.center.z;
+                         });
+
+        const uint32_t mid = low + count / 2;
+        BVH *left = partition(low, mid, depth + 1);
+        BVH *right = partition(mid, high, depth + 1);
+
+        // Compute bounding volume
+        Sphere a = get_bounding_sphere(left), b = get_bounding_sphere(right);
+        Sphere merged = bounding_sphere(&a, &b);
+
+        return new BVH(Interior{
+            .center = merged.center,
+            .radius = merged.radius,
+            .left = left,
+            .right = right,
+        });
+    };
+
+    return partition(/*low=*/0, /*high=*/spheres.size(), /*depth=*/0);
+}
+
+} // namespace
 
 // ---------------------------
 // RTIOW cuda main hook
