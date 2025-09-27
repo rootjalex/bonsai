f97910bb9eb88d502e0f5e6a40fd2e36fd3ad25c
Chris Gyurgyik <gyurgyikcp@gmail.com>
2025-09-26 18:46:10 -0700

    x

--- apps/rtiow/cuda/test.sh
+++ apps/rtiow/cuda/test.sh
@@ -19,11 +19,11 @@ for LAYOUT in "${LAYOUTS[@]}"; do
   # Compile
   rm -f ${PREFIX}/${APPLICATION}-${LAYOUT}.ppm
   cmake --build build --config Debug -j
-  ./build/compiler -i ${KERNEL_PATH}/main.bonsai -l ${PREFIX}/${LAYOUT}.bonsai -b cuda -o ${PREFIX}/rtiow.h
+  # ./build/compiler -i ${KERNEL_PATH}/main.bonsai -l ${PREFIX}/${LAYOUT}.bonsai -b cuda -o ${PREFIX}/rtiow.h
   module load cuda
   nvcc -Iapps/rtiow -Iruntime/CUDA -O3 ${PREFIX}/main.cu -o ${PREFIX}/main
   # Run
-  time ./${PREFIX}/main ${PREFIX}/${APPLICATION}-${LAYOUT}.ppm
+  compute-sanitizer ./${PREFIX}/main ${PREFIX}/${APPLICATION}-${LAYOUT}.ppm
 
   # Clean up
   rm $PREFIX/rtiow.h
