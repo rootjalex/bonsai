169e49107f2f0c7d372be254159307fb7a400e38
Chris Gyurgyik <gyurgyikcp@gmail.com>
2025-10-02 11:08:26 -0700

    x

--- apps/rt/cpu/main_trace_8.cpp
+++ apps/rt/cpu/main_trace_8.cpp
@@ -90,7 +90,8 @@ void run_test(const std::string &object, bool is_single_threaded) {
     free_canonical_tree_8(canonical_tree);
 
     std::vector<int64_t> ray_counts = {
-        1 << 15, 1 << 16, 1 << 17, 1 << 18, 1 << 19, 1 << 20,
+        1 << 10, 1 << 11, 1 << 12, 1 << 13, 1 << 14, 1 << 15, 1 << 16, 1 << 17,
+        1 << 18, 1 << 19, 1 << 20, 1 << 21, 1 << 22, 1 << 23, 1 << 24,
     };
     bool is_first_run = true;
     for (const int64_t ray_count : ray_counts) {
