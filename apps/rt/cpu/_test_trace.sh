8c9d1bfa239aefb8114017946f5ee2da3971146a
Chris Gyurgyik <gyurgyikcp@gmail.com>
2025-10-02 14:07:52 -0700

    x

--- apps/rt/cpu/_test_trace.sh
+++ apps/rt/cpu/_test_trace.sh
@@ -144,9 +144,9 @@ run_tests() {
       COMPILE="${COMMON_FLAGS} -o ${PREFIX}/${APPLICATION}_${LAYOUT}.out ${PREFIX}/${MAIN_FILE}.cpp ${PREFIX}/${APPLICATION}.cpp"
       if [[ "$(uname)" == "Linux" ]]; then
         # add additional flags for linking openmp...
-        COMPILE="${COMPILE} -L/scratch/cpg/miniconda3/envs/bonsai_env/lib"
+        COMPILE="${COMPILE} -L/scratch/cpg/miniconda3/envs/bonsai_env/lib -lomp"
       fi
-      echo ${COMPILE}
+      echo "clang++ ${COMPILE}"
       clang++ ${COMPILE}
       
       # 4. Run it.
