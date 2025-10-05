7718434bd4acc0a39baa91080dcd3c0a3147e554
Chris Gyurgyik <gyurgyikcp@gmail.com>
2025-10-05 09:40:01 -0700

    x

--- apps/rt/cuda/main_trace.cu
+++ apps/rt/cuda/main_trace.cu
@@ -130,13 +130,13 @@ void run(const std::string &object, const std::vector<int64_t> &ray_counts) {
     using clock = std::chrono::high_resolution_clock;
     std::vector<Triangle> triangles = load_obj(object);
     assert(!triangles.empty());
-    std::cout << "object loaded\n";
+    std::cout << "object loaded" << std::endl;
     BVH *canonical_tree = build_canonical_tree_$N$(triangles);
-    std::cout << "canonical tree built\n";
+    std::cout << "canonical tree built" << std::endl;
     Triangles tree = build_triangles(canonical_tree);
-    std::cout << "specialized tree built\n";
+    std::cout << "specialized tree built" << std::endl;
     free_canonical_tree_$N$(canonical_tree);
-    std::cout << "canonical tree freed\n";
+    std::cout << "canonical tree freed" << std::endl;
 
     bool is_first_run = true;
     for (const int64_t ray_count : ray_counts) {
