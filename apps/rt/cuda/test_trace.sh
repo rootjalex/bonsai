a24aa509a001e6c1dfd5eb9e3ef32c5c94c36a1c
Chris Gyurgyik <gyurgyikcp@gmail.com>
2025-10-05 09:03:19 -0700

    x

--- apps/rt/cuda/test_trace.sh
+++ apps/rt/cuda/test_trace.sh
@@ -126,7 +126,7 @@ run_tests() {
       # 1. Build the Bonsai compiler.
       cmake --build build --config Debug -j > /dev/null
       # 2. Lower to cuda.
-      # ./build/compiler -i ${PREFIX}/main.bonsai -l ${PREFIX}/${LAYOUT}.bonsai -b cuda -o ${PREFIX}/${APPLICATION}.h
+      ./build/compiler -i ${PREFIX}/main.bonsai -l ${PREFIX}/${LAYOUT}.bonsai -b cuda -o ${PREFIX}/${APPLICATION}.h
       # 3. Compile the lowered cuda.
       module load cuda
       nvcc -Iapps/rt -Iruntime/CUDA -O3 ${PREFIX}/${MAIN_FILE}.cu -o ${PREFIX}/${APPLICATION}_${LAYOUT}.out
