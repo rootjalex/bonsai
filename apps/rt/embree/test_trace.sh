b55c76e8ae8bab8e2094199858b8d7ce8c5195d7
Chris Gyurgyik <gyurgyikcp@gmail.com>
2025-10-10 20:40:27 -0700

    x

--- apps/rt/embree/test_trace.sh
+++ apps/rt/embree/test_trace.sh
@@ -53,7 +53,7 @@ PARTITION="sah"
 echo "${N}, ${RAY_TYPE}, ${SCHEDULE}"
 
 MIN_POWER=15
-MAX_POWER=20
+MAX_POWER=22
 
 # only run on performance cores for the Fredwood.
 FREDWOOD_FLAG="numactl --physcpubind 0-15" 
