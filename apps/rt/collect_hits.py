660e2e1f7d9988d508a9b0264001d2c903301fb9
Chris Gyurgyik <gyurgyikcp@gmail.com>
2025-10-24 09:12:25 -0700

    fix name

--- apps/rt/collect_hits.py
+++ apps/rt/collect_hits.py
@@ -6,12 +6,11 @@ import numpy as np
 DEFAULT_LAYOUT_GROUPS = {
     'bvh8': ['bvh8', 'bvh8-align16', 'cl-bvh8', 'cl-bvh8-align16',
              'cl-bvh8-idx', 'cl-bvh8-idx-align16',
-             # 'cl-cw-bvh8-idx', 'cl-cw-bvh8-idx-align16',
              ],
     'bvh2': ['eq', 'pbrt', 'pbrt-align16', 'eq-align16',
              'ptr', 'soaos-align16', 'soaos',
              ],
-    'embree': ['embree-bvh4', 'embree-bvh8'],
+    'embree': ['embree-qbvh8i', 'embree-qbvh8'],
 }
 
 
