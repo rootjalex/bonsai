4a693adb5e30620c61f6c3dac9e3795c8b64f7e0
Chris Gyurgyik <gyurgyikcp@gmail.com>
2025-10-26 08:46:09 -0700

    x

--- apps/rt/layout_grouping.py
+++ apps/rt/layout_grouping.py
@@ -1,6 +1,6 @@
 layout_groups = {
     'bvh8': ['bvh8', 'bvh8-align16', 'bvh8-q8',
-             'bvh8-q16', 'bvh8-q8-align16', 'bvh8-q8-ci-align16',
+             'bvh8-q16', 'bvh8-q16-ci', 'bvh8-q8-align16', 'bvh8-q8-ci-align16',
              'bvh8-q8-ci', 'bvh8-q16-align16', 'bvh8-q16-ci-align16'],
     'bvh2': ['sg-eq', 'pbrt', 'pbrt-align16', 'sg-eq-align16',
              'ptr', 'pbrt-soaos', 'pbrt-soaos-align16',
