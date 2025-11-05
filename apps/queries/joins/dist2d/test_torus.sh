5ffc5b725bc4029b5585764ea83e494a54d20cec
Alexander <ajroot@stanford.edu>
2025-11-04 22:15:41 -0800

    buff up torus join

--- apps/queries/joins/dist2d/test_torus.sh
+++ apps/queries/joins/dist2d/test_torus.sh
@@ -6,9 +6,9 @@ cmake --build build --config Debug -j
 
 SRC_DIR=apps/queries/joins/dist2d
 
-time ./build/compiler -i $SRC_DIR/joins.bonsai -p canonicalize -p lower-trees -p lower-externs -p lower-dynamic-sets -p lower-scans -p lower-layouts -p loop-transforms -p lower-recloops -p lower-foreachs -p unswitch -p lower-yields -p simplify -p lower-logical-operation -p cse -p dce -b cppx -o $SRC_DIR/joins_gen
+time ./build/compiler -i $SRC_DIR/torus.bonsai -p canonicalize -p lower-trees -p lower-externs -p lower-dynamic-sets -p lower-scans -p lower-layouts -p loop-transforms -p lower-recloops -p lower-foreachs -p unswitch -p lower-yields -p simplify -p lower-logical-operation -p cse -p dce -b cppx -o $SRC_DIR/torus_gen
 
-time clang++ -std=c++20 -O3 -g -o $SRC_DIR/main_torus.out $SRC_DIR/main_torus.cpp $SRC_DIR/joins_gen.cpp -I.
+time clang++ -std=c++20 -O3 -g -o $SRC_DIR/main_torus.out $SRC_DIR/main_torus.cpp $SRC_DIR/torus_gen.cpp -I.
 
 if [[ "$(uname)" == "Linux" ]]; then
     BIND="numactl --physcpubind 0-15"
@@ -17,5 +17,7 @@ else
 fi
 
 $BIND ./$SRC_DIR/main_torus.out 1 &> $SRC_DIR/torus1_results.txt
+$BIND ./$SRC_DIR/main_torus.out 5 &> $SRC_DIR/torus5_results.txt
 $BIND ./$SRC_DIR/main_torus.out 100 &> $SRC_DIR/torus100_results.txt
 $BIND ./$SRC_DIR/main_torus.out 1000 &> $SRC_DIR/torus1000_results.txt
+$BIND ./$SRC_DIR/main_torus.out 100000 &> $SRC_DIR/torus100000_results.txt
